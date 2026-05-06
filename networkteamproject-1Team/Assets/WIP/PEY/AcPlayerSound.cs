17361c6659e741be4237ff43b7a339eaea97f5d1
Masttte <peonyak08@gmail.com>
2026-05-06 16:57:14 +0900

    chore: 착지소리 적용, 텍스쳐/셰이더 리소스 추가
    오디오 디테일 약간 조절

--- networkteamproject-1Team/Assets/WIP/PEY/AcPlayerSound.cs
+++ networkteamproject-1Team/Assets/WIP/PEY/AcPlayerSound.cs
@@ -1,18 +1,26 @@
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Audio;
 
 // AnimationEvent를 받아 사운드 재생
-public class AcPlayerSound : MonoBehaviour
+public class AcPlayerSound : NetworkBehaviour
 {
     [SerializeField] AudioResource _footStep;
+    [SerializeField] AudioResource _land;
 
-    private void OnFootstep(AnimationEvent animationEvent)
+    public void OnFootstep(AnimationEvent animationEvent)
     {
         AudioManager.Instance.PlaySfxWet(_footStep, this.transform.position);
     }
-
-    private void OnLand(AnimationEvent animationEvent)
+    public void OnLand(AnimationEvent animationEvent)
     {
+        if (!IsOwner) return; // 착지 이벤트는 오너가 아니면 가끔 안돼서, 아예 오너만 받고 ClientRpc 연결
+        OnLandRpc();
+    }
 
+    [Rpc(SendTo.Everyone)]
+    public void OnLandRpc()
+    {
+        AudioManager.Instance.PlaySfxWet(_land, this.transform.position);
     }
 }
