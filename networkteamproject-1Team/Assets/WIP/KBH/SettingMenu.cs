26b52a60602ebd0553d18be5ade019575c3dfae5
23M-RFT68 <kobh0221@gmail.com>
2026-05-06 15:40:12 +0900

    [feat] 설정 스크립트 생성

---
+++ networkteamproject-1Team/Assets/WIP/KBH/SettingMenu.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class SettingMenu : MonoBehaviour
+{
+    // Pause 메뉴 패널
+    [Header("Pause 메뉴")]
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button settingsButton;
+    [SerializeField] private Button quitButton;
+    
+    // 설정 패널
+    [Header("설정")]
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private Button backButton;
+
+    [Header("사운드 설정")] 
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider sfxSlider;
+    [SerializeField] private TextMeshProUGUI bgmValueText;
+    [SerializeField] private TextMeshProUGUI sfxValueText;
+    
+    
+    
+    
+    
+}
